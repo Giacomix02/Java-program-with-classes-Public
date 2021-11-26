e83cb19d316d4f24b9f39fb935af6ae43849f95a
Giacomo <paoloccigiacomo@gmail.com>
2021-11-26 09:44:44 +0100

    little fix and changes

--- EsercizioStudenti.java
+++ EsercizioStudenti.java
@@ -1,4 +1,5 @@
 package nostro;
+
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,30 +11,31 @@ import java.util.*;
     nome,cognome,facolta,id
 
     CLASSI:
+    ID,capienza,corso
     
 */
 public class EsercizioStudenti {
     public static void main(String[] args) throws IOException {
+
       
 
         //da testo gli alunni
         String testoAlunni = Files.readString(Paths.get("studenti.txt"));
         String[] righeAlunni = testoAlunni.split("\n");
-
         Studente[] studenti = new Studente[righeAlunni.length]; //dichiaro vettore studenti
         
-        for(int i = 0; i<righeAlunni.length && i<studenti.length; i++){
+        for(int i = 0; i<righeAlunni.length; i++){
             String[] riga = righeAlunni[i].split(","); //[nome,cognome,facolta,id]
             studenti[i] = new Studente(riga[0],riga[1],riga[2],Integer.parseInt(riga[3]));
         }
 
+
         //da testo le aule
         String testoAule = Files.readString(Paths.get("aule.txt"));
         String[] righeAule = testoAule.split("\n");
-
         Aula[] aule = new Aula[righeAule.length];   //dichiaro vettore aule
    
-        for(int i = 0; i<righeAule.length && i<aule.length; i++){
+        for(int i = 0; i<righeAule.length; i++){
             String[] riga = righeAule[i].split(",");
             aule[i] = new Aula(
                 Integer.parseInt(riga[0]),
@@ -50,23 +52,55 @@ public class EsercizioStudenti {
                 } 
             }
         }
-        for(int i=0;i<aule.length;i++){
-            System.out.println(aule[i]);
-            System.out.println("_______________________");
-        }
-    }
 
-    public static void stampaClassi(Aula[] classi){
-        stampaClassi(classi,0);
-    }
-    
-    public static void stampaClassi(Aula[] classi, int i) {
-        if (i < classi.length) {
-            System.out.println(classi[i]);
-            System.out.println("-----------------");
-            i++;
-            stampaClassi(classi, i);
-        }
+
+        Scanner sc=new Scanner(System.in);  
+        int scelta = 0;
+        boolean esci = false;
+        do{
+
+            System.out.print("what you want do?\n\t1-Search a student by id\n\t2-See all the classrooms\n>> ");
+            scelta = sc.nextInt();
+            if (scelta == 1) {
+                System.out.print("___________________\n\nInsert student ID \n>> ");
+                int idSearch = sc.nextInt();
+
+                for(int i=0; i<studenti.length;i++){
+                    if(studenti[i].getId()==idSearch){
+                        System.out.println(studenti[i]);
+                        break;
+                    }
+                }
+
+            }
+
+            
+            if (scelta == 2) {
+                for (int i = 0; i < aule.length; i++) { // stampo a schermo aule con relativi studenti
+                    System.out.println(aule[i]);
+                    System.out.println("_______________________");
+                }
+            }
+
+
+
+            System.out.print("Do you whant to exit? [Y/n]\n>> ");
+            char esciChar = sc.next().charAt(0);
+
+
+            if(esciChar=='Y'||esciChar=='y'){
+                break;
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+
+           
+
+        }while(true);
+
+        sc.close();
+
     }
 
 }
@@ -115,53 +149,46 @@ class Aula {
     public Aula(int numClasse, int capienza, String corso) {
         this.idClasse = numClasse;
         this.capienza = capienza;
-        this.corso=corso;
+        this.corso = corso;
     }
 
-    public void aggiungiStudente(Studente studente){
-        //TODO se ci sono piu studenti di quanto è la capacità, crea un errore
+    public void aggiungiStudente(Studente studente) {
+        // TODO se ci sono piu studenti di quanto è la capacità, crea un errore
         this.studenti.add(studente);
     }
-    
-    public int getcapienza() { 
+
+    public int getcapienza() {
         return this.capienza;
     }
 
-    public int getClasse() { 
+    public int getClasse() {
         return this.idClasse;
     }
 
-    public String getCorso() { 
+    public String getCorso() {
         return corso;
     }
 
-    public String toString(){  
+    public String toString() {
         int numStudenti = studenti.size();
-        String output = 
-            "Classe: "+idClasse+"\n"+
-            "Indirizzo: "+corso+"\n"+
-            "Capienza: "+capienza+"\n"+
-            "Posti liberi: "+(capienza-numStudenti)+"\n\n"+
-            "---Alunni---\n";
-        for(int i=0;i<numStudenti;i++){
-            output+=studenti.get(i);
+        String output = "Classe: " + idClasse + "\n" + "Indirizzo: " + corso + "\n" + "Capienza: " + capienza + "\n"
+                + "Posti liberi: " + (capienza - numStudenti) + "\n\n" + "---Alunni---\n";
+        for (int i = 0; i < numStudenti; i++) {
+            output += studenti.get(i);
         }
-        return output; 
+        return output;
     }
-    public void stampaStudenti(){
-        stampaStudenti(studenti,0);
+
+    public void stampaStudenti() {
+        stampaStudenti(studenti, 0);
     }
 
     public void stampaStudenti(Vector<Studente> arrStud, int i) {
         if (i < studenti.size()) {
-            System.out.print((i+1)+") "); 
+            System.out.print((i + 1) + ") ");
             System.out.println(arrStud.get(i));
             i++;
             stampaStudenti(arrStud, i);
         }
     }
 }
-
-
-
-
